703a8d6592b2b8d69b63e2b40592e90d5a79516b
shakil islam <freelancershakil913@gmail.com>
2021-12-24 17:34:56 +0600

    method chainig

---
+++ chaining.php
@@ -0,0 +1,18 @@
+
+<?php 
+
+class php{
+    
+public function framework(){
+    echo "Cake PHP is a Framework.";
+    return $this;
+}
+
+public function cms(){
+    echo "Wordpress is a CMS.";
+    return $this;
+}
+
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+
+include "chaining.php";
+
+$php = new php();
+$php->framework()->cms();
+
+?>
